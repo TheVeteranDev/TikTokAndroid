4634a9bea9a6c8ef1e95a046b4d5091a2abf2711
johnrukstalis <theveterandev@gmail.com>
2025-04-15 00:09:24 -0400

    added missed field of username to comment

--- app/src/main/java/com/example/tiktokandroid/ui/post/Comment.kt
+++ app/src/main/java/com/example/tiktokandroid/ui/post/Comment.kt
@@ -2,8 +2,9 @@ package com.example.tiktokandroid.ui.post
 
 data class Comment(
     var id: String,
+    var username: String,
     var text: String,
     var likeCount: Int
 ) {
-    constructor(): this("", "", 0)
+    constructor(): this("", "", "", 0)
 }
